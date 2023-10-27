738ae6a130f8be4cc9b3a77c1131bfd8832aa7ce
dannyjll <93415718+dannyjll@users.noreply.github.com>
2023-10-26 19:59:06 -0500

    Added parsers to settings.

--- TaskList/settings.py
+++ TaskList/settings.py
@@ -130,6 +130,12 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
+    'DEFAULT_PARSER_CLASSES': [
+        'rest_framework.parsers.JSONParser',
+        'rest_framework.parsers.FormParser',
+        'rest_framework.parsers.MultiPartParser',
+        'rest_framework.parsers.FileUploadParser',
+    ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
